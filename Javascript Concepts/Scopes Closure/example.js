a871c6bc3d3876b7b8076b2b08e8f0be3d7af74a
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-01-13 11:48:46 +0530

    notes added

--- Javascript Concepts/Scopes Closure/example.js
+++ Javascript Concepts/Scopes Closure/example.js
@@ -4,13 +4,28 @@ const info = {
     gmail : 'deepak@gmail.com',
     working: 'Fullstack Developer / Mern Stack Developer'
 }
-info.name = "rahul"
-info.fullname = "kl rahul"
-console.log(info)
+// info.name = "rahul"
+// info.fullname = "kl rahul"
+//console.log(info)
 
 
 
-console.log(info)
+//console.log(info)
 //console.log(Object.keys(info))
 //console.log(Object.values(info))
 //console.log(Object.entries(info))
+
+//console.log(Object.entries(info).length)
+
+const elements = Object.keys(info)
+Object.preventExtensions(info)
+for(let i=0; i<elements.length; i++){
+    Object.defineProperty(info, elements[i], {writable:false, configurable:false})
+}
+
+info.name = "rahul"
+info.fullname = "kl rahul"
+info.location = "Odisha"
+
+console.log(delete info.name)
+console.log(info)
