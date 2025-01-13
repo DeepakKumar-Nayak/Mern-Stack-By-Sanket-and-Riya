f1c59aeb2f3be2be825cf48f0a983538c379e313
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-01-13 10:48:27 +0530

    notes added

---
+++ Javascript Concepts/Scopes Closure/example.js
@@ -0,0 +1,16 @@
+const info = {
+    name :'deepak',
+    fullname : 'deepak kumar nayak',
+    gmail : 'deepak@gmail.com',
+    working: 'Fullstack Developer / Mern Stack Developer'
+}
+info.name = "rahul"
+info.fullname = "kl rahul"
+console.log(info)
+
+
+
+console.log(info)
+//console.log(Object.keys(info))
+//console.log(Object.values(info))
+//console.log(Object.entries(info))
