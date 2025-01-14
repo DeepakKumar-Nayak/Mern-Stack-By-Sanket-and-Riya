56a3d3aa7afaabbb59e6a54592c583f84d4a15ad
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-01-14 19:46:15 +0530

    notes added

--- Javascript Concepts/Scopes Closure/example.js
+++ Javascript Concepts/Scopes Closure/example.js
@@ -17,15 +17,37 @@ const info = {
 
 //console.log(Object.entries(info).length)
 
-const elements = Object.keys(info)
-Object.preventExtensions(info)
-for(let i=0; i<elements.length; i++){
-    Object.defineProperty(info, elements[i], {writable:false, configurable:false})
-}
+// const elements = Object.keys(info)
+// Object.preventExtensions(info)
+// for(let i=0; i<elements.length; i++){
+//     Object.defineProperty(info, elements[i], {writable:false, configurable:false})
+// }
 
-info.name = "rahul"
-info.fullname = "kl rahul"
-info.location = "Odisha"
+// info.name = "rahul"
+// info.fullname = "kl rahul"
+// info.location = "Odisha"
 
 //console.log(delete info.name)
 //console.log(info)
+
+function getName(){
+    let name = "deepak_kumar_nayak"
+
+    function getdataOne(){
+        var surname = "nayak"
+        console.log(name) // can be accessible
+    }
+    function getDatTwo(){
+        let favGame = "Carrom"
+        console.log(name) // can be accessible
+        console.log(surname) // not accessible
+    }
+    function getdataThree(){
+        console.log(favGame) // not accessible
+        console.log(name) // can be accessible
+    }
+    getdataOne()
+    getDatTwo()
+    getdataThree()
+}
+getName()
