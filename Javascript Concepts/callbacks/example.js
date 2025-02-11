f355e579e5e13ea507f2706187c51478c0613713
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-02-11 07:11:53 +0530

    notes added

--- Javascript Concepts/callbacks/example.js
+++ Javascript Concepts/callbacks/example.js
@@ -1,7 +1,19 @@
 arrayOne = [10,20,30,40,50]
 
-const data = arrayOne.map(function getElement(element){
-    return element*2
-})
+// const data = arrayOne.map(function getElement(element){
+//     return element*2
+// })
 
-console.log(data)
+// console.log(data)
+
+function getResult(arr, fun){
+    let result = []
+    for(let i=0; i<arr.length; i++){
+        result.push(fun(arr[i]))
+    }
+    return result;
+}
+
+getResult(arrayOne, function getData(element){
+    return element**2
+})
