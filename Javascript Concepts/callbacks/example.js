2702608339ea0071afcbcd19e73f1fbdf538cb1b
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-02-12 17:18:47 +0530

    code added

--- Javascript Concepts/callbacks/example.js
+++ Javascript Concepts/callbacks/example.js
@@ -6,14 +6,36 @@ arrayOne = [10,20,30,40,50]
 
 // console.log(data)
 
-function getResult(arr, fun){
-    let result = []
-    for(let i=0; i<arr.length; i++){
-        result.push(fun(arr[i]))
+// function getResult(arr, fun){
+//     let result = []
+//     for(let i=0; i<arr.length; i++){
+//         result.push(fun(arr[i]))
+//     }
+//     return result;
+// }
+
+// getResult(arrayOne, function getData(element){
+//     return element**2
+// })
+
+// setTimeout(function getData(){
+//     console.log('hello')
+// },100)
+
+// let x = 0; 
+// for(let i=0; i<=10; i++){
+//     x=x+1 
+// }
+// console.log(x)
+
+// cosure example: 
+function func(a,c){
+    let m = 10
+    function gun(){
+        console.log(m,c)
     }
-    return result;
+    return gun;
 }
 
-getResult(arrayOne, function getData(element){
-    return element**2
-})
+const g = func(10,20)
+g()
