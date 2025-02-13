b0134202df487e52156a99908e17d6694ec1f3fe
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-02-13 08:49:03 +0530

    code added for promise

--- Javascript Concepts/callbacks/example.js
+++ Javascript Concepts/callbacks/example.js
@@ -29,13 +29,50 @@ arrayOne = [10,20,30,40,50]
 // console.log(x)
 
 // cosure example: 
-function func(a,c){
-    let m = 10
-    function gun(){
-        console.log(m,c)
-    }
-    return gun;
+// function func(a,c){
+//     let m = 10
+//     function gun(){
+//         console.log(m,c)
+//     }
+//     return gun;
+// }
+
+// const g = func(10,20)
+// g()
+
+
+function download(url,callback){
+    console.log('downloading data from the url', url)
+    setTimeout(()=>{
+        console.log('downloading data from the url is completed')
+        let Downloaddata = "someData"
+        callback(Downloaddata)
+    },3000)
 }
 
-const g = func(10,20)
-g()
+function writeFile(data, filename, callback){
+    console.log('writting',data,'to the file',filename)
+    setTimeout(()=>{
+        console.log('writting data to the file is over')
+        let status = "successfully"
+        callback(status)
+    },4000)
+}
+
+function upload(filename,link,status, callback){
+    console.log('uploading file to the',link)
+    setTimeout(()=>{
+        console.log('uploaded',filename,status)
+        let msg = "everything done successfully"
+        callback(msg)
+
+    },5000)
+}
+
+download('www.google.com', function writeData(data){
+    writeFile(data, 'file.txt', function uploadData(status){
+        upload('file.txt', 'googleDriev',status, function Over(msg){
+            console.log(msg)
+        })  
+    })
+})
