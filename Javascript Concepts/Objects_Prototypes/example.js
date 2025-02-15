6769b1b7d9df0d8c3b525098302dbe3470d062fd
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-02-15 21:51:16 +0530

    notes added

---
+++ Javascript Concepts/Objects_Prototypes/example.js
@@ -0,0 +1,28 @@
+class Product {
+
+    // writting some data members: 
+
+    name
+    price
+    category
+    description
+    quantity
+
+    // writing some member functions
+ 
+
+    addToCart(){
+        console.log('product added to the cart')
+    }
+
+    DisplayDetails(){
+        console.log('display details of the product')
+    }
+
+
+}
+
+const product = new Product() // this is how a object is created and yes this is not a fully fledged Object there are many more things to be done and we will do that on later stages. 
+
+
+
