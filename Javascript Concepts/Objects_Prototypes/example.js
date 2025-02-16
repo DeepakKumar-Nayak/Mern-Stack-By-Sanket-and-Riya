847df07cfee8591ea08016df7ba8fb7d2c625d13
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-02-16 11:49:48 +0530

    code added

--- Javascript Concepts/Objects_Prototypes/example.js
+++ Javascript Concepts/Objects_Prototypes/example.js
@@ -1,28 +1,52 @@
-class Product {
+// class Product {
 
-    // writting some data members: 
+//     // writting some data members: 
 
-    name
-    price
-    category
-    description
-    quantity
+//     name
+//     price
+//     category
+//     description
+//     quantity
 
-    // writing some member functions
+//     // writing some member functions
  
 
+//     addToCart(){
+//         console.log('product added to the cart')
+//     }
+
+//     DisplayDetails(){
+//         console.log('display details of the product')
+//     }
+
+
+// }
+
+//const product = new Product() // this is how a object is created and yes this is not a fully fledged Object there are many more things to be done and we will do that on later stages. 
+
+
+
+class Product {
+
+    constructor(productName, productCategory, productQuantity, productDescription, productRating){
+
+        this.name = productName; // Laptop
+        this.category = productCategory; // Electronics
+        this.quantity = productQuantity; // 12
+        this.description = productDescription; // Hp Laptop
+        this.rating = productRating // 4.0
+
+    }
     addToCart(){
         console.log('product added to the cart')
     }
-
-    DisplayDetails(){
-        console.log('display details of the product')
-    }
-
-
 }
 
-const product = new Product() // this is how a object is created and yes this is not a fully fledged Object there are many more things to be done and we will do that on later stages. 
+const productOne = new Product('Laptop','Electronics',12, 'Hp Laptop', '4.0')
 
+// aggar apko new data members add karna hai toh aap object ke help se kar sakhte ho called variable ko leke for example; 
 
+productOne.color = "black"
+
+console.log(productOne.name)
 
