51028bd045dee9523a88de084ee8b0319ace41b9
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-02-19 21:16:31 +0530

    code added

--- Javascript Concepts/Objects_Prototypes/bundle.js
+++ Javascript Concepts/Objects_Prototypes/bundle.js
@@ -3,6 +3,10 @@ class Product {
     #price
     #quantity
 
+    // yahan meine private data-memebers banaya hai kyun ki mein nahi chahta hoon ki koi bhi akke mere data-memebers mein idhar udhar ki value pass kar de, and private ki vajeh se bhi class ke bahar se koi value aise update nahi kar paenge jab tak ham getter and setter use na kare
+
+    
+
     constructor(builder){
         this.#name = builder.name;
         if(builder.price>0){
@@ -14,9 +18,13 @@ class Product {
         this.#quantity = builder.quantity;
     }
 
+    // if i use the approach of creating a product without conatructor then i will face a number of problems like i will not be able to do the validatuon before object creation. suppose u got a senario where before object creation you have to do some validatons aur aggar apka validation fulfill nahi hua then apka object creation bandh ho jaega and ham getter and setter bhi nahi use kar paenge toh dono issue hamko solvbe karna hai
+
     displayProduct(){
         console.log(this.#name, this.#price, this.#quantity)
     }
+
+    // this is what i have done 
     static get Builder(){
         class Builder {
             constructor(){
