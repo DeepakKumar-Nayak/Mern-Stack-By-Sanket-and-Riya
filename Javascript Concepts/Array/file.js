551f82ac720b4b4f3557f7c2527b820a750a8530
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-04-02 12:24:00 +0530

    filed added

--- Javascript Concepts/Array/file.js
+++ Javascript Concepts/Array/file.js
@@ -1,16 +1,38 @@
-let arrayOne = [1,0,0,1,1,1,0,1,0,0,1]
-let arrayTwo = []
-let arrayThree = []
+// let arrayOne = [1,0,0,1,1,1,0,1,0,0,1]
+// let arrayTwo = []
+// let arrayThree = []
 
-for(let i=0; i<arrayOne.length; i++){
-    if(arrayOne[i]== 1){
-        arrayTwo.push(arrayOne[i]-1)
-    }else{
-        arrayThree.push(arrayOne[i]+1)
+// for(let i=0; i<arrayOne.length; i++){
+//     if(arrayOne[i]== 1){
+//         arrayTwo.push(arrayOne[i]-1)
+//     }else{
+//         arrayThree.push(arrayOne[i]+1)
+//     }
+// }
+
+// arrayOne = [...arrayTwo, ...arrayThree]
+// console.log(arrayOne)
+
+// Another Way of doing it. 
+let arrayOne = [1,0,0,1,1,1,0,1,0,0,1,1,1,0,0,0,1,0,1,0,0]
+let left = 0;
+let right = arrayOne.length-1
+
+while(left<right){
+    if(arrayOne[left] === 1){
+        arrayOne[left] = 0; 
+        arrayOne[right] = 1;
+        right--;
     }
+    left++;
+
 }
 
-arrayOne = [...arrayTwo, ...arrayThree]
 console.log(arrayOne)
 
-
+0 < 10
+1< 9
+2<8
+3<7
+4<5
+5<5
