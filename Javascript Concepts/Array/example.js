e727f54f74887a661ab11ee42e49e35a0acf5d6b
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-04-06 08:09:21 +0530

    max consecutive ones solution added

--- Javascript Concepts/Array/example.js
+++ Javascript Concepts/Array/example.js
@@ -1,8 +1,16 @@
-let str = "missippi"
-let freMap = {}
-
-if(freMap['name']){
-    console.log(freMap['name'])
-}else{
-    freMap['name']= 'deepak'
+let nums =  [1,1,0,1,1,1]
+let temp = [];
+let previousValue = 0;
+let newValue = 0
+for(let i=0; i<nums.length; i++){
+    if(nums[i] === 1){
+     temp.push(nums[i])
+     if(temp.length>previousValue)
+        previousValue = temp.length
+    }else if(nums[i] === 0){
+        temp=[]
+        continue;
+        
+    }
 }
+console.log(previousValue)
