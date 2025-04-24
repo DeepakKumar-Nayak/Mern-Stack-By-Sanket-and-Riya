456a842ae12691ab6dc92227830a844b946a5c4a
Deepak Kumar Nayak <deepakkumarnk89@gmail.com>
2025-04-24 23:51:28 +0530

    code added

--- DSA_Problems/majority_element.js
+++ DSA_Problems/majority_element.js
@@ -13,4 +13,34 @@ let majority_element = function(nums){
     }
     return majorityElement;
 } 
-console.log(majority_element([3,2,3]))
+//console.log(majority_element([2,2,1,1,1,2,2]))
+
+function majorityElement(nums){
+    let freq = {}
+    for(let i of nums){
+        if(freq[i]){
+            freq[i]+=1
+        }else{
+            freq[i]= 1
+        }
+    }
+    console.log(freq)
+
+    let maxFreq = 0;
+    let majority_element = 0
+    for(let i in freq){
+       if(freq[i]> maxFreq){
+        maxFreq = freq[i]
+        majority_element = Number(i)
+       } 
+
+    }
+    // here i am checking if the element is present more than nums.length/2 times otherwise return []
+    // jitna bhi array ka length hoga aggar uska adhe se bhi jyada ho to ho gaya woh majority element
+    // for example aggar array mein 5 elements hai aur aggar majority element 2 se jyada yani 3 bar 
+    // present ho toh wohi element ban jaega majority element
+    return maxFreq > Math.floor(nums.length / 2) ? majority_element : [];
+    
+}
+
+console.log(majorityElement([1,2,3,4]))
--- DSA_Problems/square_sorted.js
+++ DSA_Problems/square_sorted.js
@@ -1,12 +1,9 @@
 let square_sorted = function(nums){
-    let result = []
-    for(let i=0; i<nums.length; i++){
-        nums[i] = nums[i]**2
-    }
-    
-    
+    let result = new Array(nums.length)
+    return result;
 }
-console.log(square_sorted([-4,1,0,3,10]))
+console.log(square_sorted([-4, 1, 0, 3, 10]));
 
 
-[16,1,0,9,100]
+//[16,1,0,9,100]
+//[0,1,9,16,100]
